41193142e49ad9bf4e938067491421ccdae1bc11
judasaca <salamancajd14@gmail.com>
2023-06-06 19:53:13 -0500

    add basic setup

---
+++ README.md
@@ -0,0 +1,3 @@
+## Quality Assurance with Java - Globant.
+
+This repository contains all the exercises proposed in the course.
---
+++ src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        exercise1();
+    }
+    public static void exercise1() {
+        System.out.println("esto es la hostia");
+    }
+}
