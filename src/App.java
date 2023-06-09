26f75173767d0677f6911c943d1c94b9b7cc6c2c
judasaca <salamancajd14@gmail.com>
2023-06-08 19:19:59 -0500

    Basic class constructor implementation.

--- src/App.java
+++ src/App.java
@@ -1,9 +1,7 @@
+import Entidad.Persona;
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
-        exercise1();
-    }
-    public static void exercise1() {
-        System.out.println("esto es la hostia");
+        Persona juan = new Persona("juan", 23, "salamanca");
     }
 }
---
+++ src/Entidad/Persona.java
@@ -0,0 +1,14 @@
+package Entidad;
+
+public class Persona {
+    public String nombre;
+    public String primerApellido;
+    public int edad; 
+
+    public Persona(String nombre, Integer edad, String primerApellido) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.primerApellido = primerApellido;
+    }
+
+}
