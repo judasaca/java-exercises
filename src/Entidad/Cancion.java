0eeb4d55997f43923de4720f916b50b3868dbd73
judasaca <salamancajd14@gmail.com>
2023-06-23 15:47:26 -0500

    Ejercicio extra 1.

---
+++ src/Entidad/Cancion.java
@@ -0,0 +1,30 @@
+package Entidad;
+
+public class Cancion {
+    private String titulo, autor;
+
+    public Cancion(){
+        
+    }
+    public Cancion(String titulo, String autor) {
+        this.titulo = titulo;
+        this.autor = autor;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+    
+}
